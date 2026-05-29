1dbedc64659efa19dc05dcd8cdcaec7d19c787d6
Kauã Pereira <borgeskaua1210@gmail.com>
2026-05-29 20:49:07 -0300

    chore: organiza estrutura base e fluxo inicial da aplicação
    Organiza a estrutura principal do projeto e fluxo inicial da aplicação.
    
    - Configuração do index.php
    - Redirecionamento automático
    - Estrutura MVC inicial
    - Organização de pastas do sistema

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-echo "FastNote funcionando!";
+header('Location: ...'); 
+    session_start();
+
+    if (isset($_SESSION['user_id'])) {
+        header('Location: dashboard.php');
+    } else {
+        header("Location: views/login.php");
+    }
+    exit();
+    
+
+   
 ?>
